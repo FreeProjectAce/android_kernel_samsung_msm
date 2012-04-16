a57179306fae745ae6ea2a5d25642b09d07889aa
Kolja Dummann <k.dummann@gmail.com>
2012-04-16 21:59:57 +0200

    copy modules to device folder after build

--- make_kernel_GT-S5830.sh
+++ make_kernel_GT-S5830.sh
@@ -1,2 +1,5 @@
 make cooper_cm_rev03_defconfig
 make -j 8
+cp arch/arm/boot/zImage ../../../device/samsung/cooper/kernel 
+cp drivers/net/tun.ko ../../../device/samsung/cooper/prebuilt/tun.ko
+cp fs/cifs/cifs.ko ../../../device/samsung/cooper/prebuilt/cifs.ko
